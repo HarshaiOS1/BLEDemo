7db90625ca1cf9e208726ec5d978ae1294f15dfe
HarshaiOS1 <46565732+HarshaiOS1@users.noreply.github.com>
2024-05-24 13:45:09 +0200

    Inital commit with TCA package added

---
+++ BLEDemo/BLEDemo/BLEDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEDemoApp.swift
+//  BLEDemo
+//
+//  Created by Krishnappa, Harsha on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BLEDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # BLEDemo
+Aim: Get list of BLE devices and show them in a list. Refresh the list for every 10 seconds.
+
+Architecture : The Composable Architecture
+ (https://github.com/pointfreeco/swift-composable-architecture)
+
